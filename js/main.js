56d675c3f8cebc12ef0c9b8d9a261cf686152585
Michele <micheleierniero@gmail.com>
2021-07-30 13:32:11 +0200

    punto tre

--- js/main.js
+++ js/main.js
@@ -13,6 +13,33 @@ for (const property in studenteCheStudia) {
 }
 
 
+// Creare un array di oggetti di studenti.
+
+var studentiAttori = [
+
+    studenteCheStudiaUno = {
+        nome: 'Eduardo',
+        cognome: 'De Filippo',
+        anni: 84,
+        interessi: ['recitazione teatrale e cinematografica'],
+    },
+    studenteCheStudiaDue = {
+        nome: 'Giuseppe',
+        cognome: 'De Filippo',
+        anni: 76,
+        interessi: ['recitazione teatrale e cinematografica'],
+    },
+    studenteCheStudiaTre = {
+        nome: 'Antonio',
+        cognome: 'De Curtis',
+        anni: 69,
+        interessi: ['recitazione teatrale e cinematografica'],
+    }
+];
+
+console.log(studentiAttori);
+  
+  
 
 
 
